693e84188567b96659e94a6edf7c1a9fa5592e16
psagion88 <sagion.pablo@gmail.com>
2025-10-22 15:00:52 -0700

    Updates

---
+++ sw.js
@@ -0,0 +1,29 @@
+const CACHE = 'txd-v1'
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.webmanifest'
+]
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()))
+})
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+    .then(() => self.clients.claim())
+  )
+})
+
+self.addEventListener('fetch', (e) => {
+  const url = new URL(e.request.url)
+  if (url.origin === location.origin) {
+    e.respondWith(
+      fetch(e.request).then(res => {
+        caches.open(CACHE).then(c => c.put(e.request, res.clone()))
+        return res
+      }).catch(() => caches.match(e.request))
+    )
+  }
+})
