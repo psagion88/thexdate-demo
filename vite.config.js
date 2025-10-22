7afd3dedc2e856909fc955ce55f7b8542d970ca3
psagion88 <sagion.pablo@gmail.com>
2025-10-22 15:59:22 -0700

    Vercel: base=/

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/thexdate-demo/', // <- MUST match your repo name
+  base: './', // relative paths so GitHub Pages works
 })
