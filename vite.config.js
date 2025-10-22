ed2fd54678db2b6d48a5ee096e751a0ebdc94efa
psagion88 <sagion.pablo@gmail.com>
2025-10-22 16:53:53 -0700

    Fix package.json; GH Pages + Vite base

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// Use VITE_BASE if provided, otherwise default to '/'
 const base = process.env.VITE_BASE || '/'
 
 export default defineConfig({
   plugins: [react()],
-  base,
+  base
 })
